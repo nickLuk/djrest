df19e954de1fe44dfdef8124704c10474a9b8a83
nickLuk <lukyanik@ukr.net>
2020-07-05 13:20:32 +0300

    start with show

--- usermanager/frontend/src/components/App.js
+++ usermanager/frontend/src/components/App.js
@@ -1,39 +1,55 @@
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 
+
+// Components
+import UsersList from "../components/usersList/usersList";
+
 class App extends React.Component {
+  apiURL = "http://localhost:8000/api/users/";
+
+  state = {
+    UsersList: [],
+  };
+
+  componentDidMount() {
+    this.updateService();
+  }
+
+  updateService() {
+    fetch(this.apiURL)
+      .then((request) => {
+        return request.json();
+      })
+      .then((data) => {
+        this.setState({
+          UsersList: data,
+        });
+      })
+      .catch((err) => console.log(err.message));
+  }
+
+  onDeleteItem = (id) => {
+    // console.log("onDeleteItem ", id);
+    // console.log(this.apiURL + id);
+    fetch(this.apiURL + id, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        this.updateService();
+        console.log(response);
+      })
+      .catch((err) => console.log(err.message));
+  };
+
   render() {
+    // console.log("State => ", this.state.UsersList);
     return (
       <Fragment>
-        <div className="outer-container">
-          <h1>User Manager Table</h1>
-          <table>
-            <thead>
-              <tr>
-                <th>id</th>
-                <th>name</th>
-                <th>email</th>
-                <th>message</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="priority-200">
-                <td className="name">TESmart KVM Switch HDMI Device</td>
-                <td className="clicks">857</td>
-                <td className="priority">
-                  <i className="fas fa-circle"></i> 200
-                </td>
-                <td className="impressions">190</td>
-                <td className="delete">
-                  <button className="delete-btn">
-                    <i className="fas fa-trash-alt" title="delete row"></i>
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <UsersList
+          UsersList={this.state.UsersList}
+          onDeleteItem={this.onDeleteItem}
+        />
       </Fragment>
     );
   }
---
+++ usermanager/frontend/src/components/usersList/userItem/userItem.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+class UserItem extends React.Component {
+  state = {
+    id: this.props.id,
+    name: this.props.name,
+    email: this.props.email,
+    message: this.props.message,
+    created_at: this.props.created_at,
+  };
+  render() {
+    const { id, name, email, message, created_at } = this.state;
+    // console.log("singleUserState => ", this.state);
+    //console.log(this.props);
+    return (
+      <tbody>
+        <tr className="priority-200">
+          <td className="name">{id}</td>
+          <td className="clicks">{name}</td>
+          <td className="priority">{email}</td>
+          <td className="impressions">{message}</td>
+          <td className="delete">
+            <button className="delete-btn" onClick={this.props.onDeleteItem}>
+              <i className="fas fa-trash-alt" title="delete row"></i>
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    );
+  }
+}
+
+export default UserItem;
